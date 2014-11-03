61fc2cd6032b881b9c2cd65eb94626986190249b
Kristina <striegnk@union.edu>
2014-11-03 08:58:53 -0500

    logging resetting of level

--- Game/assets/js/level.js
+++ Game/assets/js/level.js
@@ -12,50 +12,51 @@ var levelHints;
 var hintsManager;
 
 Crafty.scene("level", function(){
-	
-	/*Clear all objects*/
-	blocksPlaced = [];
-	portals1 = [];
-	portals2 = [];
-	
-	Crafty.load([background], function(){
-		Crafty.background("#ffffff url("+background+") no-repeat");
+    
+    /*Clear all objects*/
+    blocksPlaced = [];
+    portals1 = [];
+    portals2 = [];
+    
+    Crafty.load([background], function(){
+	Crafty.background("#ffffff url("+background+") no-repeat");
 
-		Crafty.e("2D, DOM, Image, Mouse")
-			.attr({
-				x:BOARD_WIDTH + CELL_SIZE, 
-				y:BOARD_HEIGHT - 2*CELL_SIZE,
-				w:100,
-				h:40,
-				z:1
-			})
-			.image("/images/reset.png")
-			.bind("Click", function(){
-				socket.emit("restart");
-			});
-		
-		drawLevel();
-		drawHints();
+	Crafty.e("2D, DOM, Image, Mouse")
+	    .attr({
+		x:BOARD_WIDTH + CELL_SIZE, 
+		y:BOARD_HEIGHT - 2*CELL_SIZE,
+		w:100,
+		h:40,
+		z:1
+	    })
+	    .image("/images/reset.png")
+	    .bind("Click", function(){
+		gameLog("Level Reset");
+		socket.emit("restart");
+	    });
 	
-		mode.setupLevel();
-	});
+	drawLevel();
+	drawHints();
 	
+	mode.setupLevel();
+    });
+    
 }, function(){
-	hintsManager.destroy();
+    hintsManager.destroy();
 });
 
 function advance(backgroundImage, instruction, parsedMap1, parsedMap2){
-	map 		= parsedMap1;
-	map2		= parsedMap2;
-	background 	= backgroundImage;
-	
-	if(instruction)
-		$("#data_received").html("<b class=\"gameinfo\">" + instruction +"</b>");
-	
-	level++;
-	
-	gameLog("Level Started");
-	
-	Crafty.scene("level");
+    map 		= parsedMap1;
+    map2		= parsedMap2;
+    background 	= backgroundImage;
+    
+    if(instruction)
+	$("#data_received").html("<b class=\"gameinfo\">" + instruction +"</b>");
+    
+    level++;
+    
+    gameLog("Level Started");
+    
+    Crafty.scene("level");
 }
 
